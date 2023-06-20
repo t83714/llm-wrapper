1757a494fa56fbd624c836674d2ace6cf985d64b
Jacky Jiang <t83714@gmail.com>
2023-06-20 20:48:36 +1000

    stream delegation client logic

--- http-service-agent/src/DispatchClient.ts
+++ http-service-agent/src/DispatchClient.ts
@@ -2,9 +2,14 @@ import http2 from "node:http2";
 import { DEFAULT_USERNAME } from "./middlewares/basicAuth.js";
 import JsonlParser from "stream-json/jsonl/Parser.js";
 import createCommandData, {
-    CommandDataType
+    CommandDataType,
+    DelegateRequestCommandDataType
 } from "./utils/createCommandData.js";
 import { ControlCommands } from "./utils/constants.js";
+import { Readable } from 'node:stream';
+import {
+    ReadableStream,
+  } from 'node:stream/web';
 
 const {
     HTTP2_HEADER_PATH,
@@ -16,6 +21,7 @@ const {
 interface DispatchClientConfigOptions {
     accessKey?: string;
     dispatchServerBaseUrl: URL;
+    targetServerBaseUrl: URL;
     peerMaxConcurrentStreams?: number;
 }
 
@@ -30,13 +36,18 @@ class DispatchClient {
                 "requires dispatchServerBaseUrl to be an `URL` instance!"
             );
         }
+        if (!(options.targetServerBaseUrl instanceof URL)) {
+            throw new Error(
+                "requires targetServerBaseUrl to be an `URL` instance!"
+            );
+        }
         this.options = options;
     }
 
     async start() {
         this.cleanUp();
         this.session = await this.connect();
-        this.stream = await this.request(this.session);
+        this.stream = await this.requestControlStream();
 
         const parser = new JsonlParser();
         const pipeline = this.stream.pipe(parser);
@@ -59,6 +70,21 @@ class DispatchClient {
                     createCommandData(ControlCommands.ping, "client")
                 );
                 break;
+            case ControlCommands.delegateRequest:
+                this.delegateRequest(
+                    commandData as DelegateRequestCommandDataType
+                );
+                break;
+            default:
+                throw new Error(
+                    `Invalid command type: ${
+                        commandData?.type
+                    } received. \nCommand data: ${JSON.stringify(
+                        commandData,
+                        undefined,
+                        2
+                    )}`
+                );
         }
     }
 
@@ -88,7 +114,12 @@ class DispatchClient {
         return session;
     }
 
-    async request(session: http2.ClientHttp2Session) {
+    async requestControlStream() {
+        if (!this.session) {
+            throw new Error(
+                "Cannot request control stream before creating session."
+            );
+        }
         const basePathname = this.options.dispatchServerBaseUrl.pathname;
         const requestPath =
             basePathname === "/" ? "/__bind" : `${basePathname}/__bind`;
@@ -111,10 +142,65 @@ class DispatchClient {
             ).toString("base64")}`;
         }
 
-        this.stream = session.request(headers, {
+        this.stream = this.session.request(headers, {
             endStream: false
         });
         return this.stream;
+    }
+
+    async delegateRequest(commandData: DelegateRequestCommandDataType) {
+        if (!this.session) {
+            throw new Error(
+                "Cannot delegate requests before creating session."
+            );
+        }
+        const targetRequestUrl = `${this.options.targetServerBaseUrl}${
+            this.options.targetServerBaseUrl.pathname === "/"
+                ? commandData.path.replace(/^\//, "")
+                : commandData.path
+        }`;
+
+        const requestConfig: RequestInit = {
+            method: commandData.method
+        };
+        if (commandData.data) {
+            requestConfig.body = JSON.stringify(requestConfig.body);
+        }
+        if (commandData.headers) {
+            requestConfig.headers = commandData.headers;
+        }
+
+        const res = await fetch(targetRequestUrl, requestConfig);
+
+        const basePathname = this.options.dispatchServerBaseUrl.pathname;
+        const exchangeResponseRequestPath =
+            basePathname === "/"
+                ? commandData.responsePath
+                : `${basePathname}/${commandData.responsePath}`;
+
+        const headers = {
+            [HTTP2_HEADER_METHOD]: "POST",
+            [HTTP2_HEADER_PATH]: exchangeResponseRequestPath,
+            "delegate-status-code": res.status,
+            "delegate-status-text": res.statusText,
+            ...(res.headers.get("Content-Type")
+                ? {
+                      [HTTP2_HEADER_CONTENT_TYPE]:
+                          res.headers.get("Content-Type")
+                  }
+                : {})
+        } as Record<string, string>;
+
+        if (!res.body) {
+            this.session.request(headers, {
+                endStream: true
+            });
+        } else {
+            const stream = this.session.request(headers, {
+                endStream: false
+            });
+            Readable.fromWeb(res.body as ReadableStream).pipe(stream);
+        }
     }
 
     cleanUp() {
--- http-service-agent/src/utils/createCommandData.ts
+++ http-service-agent/src/utils/createCommandData.ts
@@ -12,6 +12,16 @@ export interface CommandDataType {
     [key: string]: any;
 }
 
+export interface DelegateRequestCommandDataType extends CommandDataType {
+    method: string;
+    path: string;
+    // request data
+    data?: any;
+    headers?: Record<string, string>;
+    // a new request to dispatch server for passing the response
+    responsePath: string;
+}
+
 const createCommandData = (
     type: (typeof ControlCommands)[keyof typeof ControlCommands],
     role: CommandRoleType,
