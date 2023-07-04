70828f8656022be68a924b7f2320b262b35b3ebd
Jacky Jiang <t83714@gmail.com>
2023-07-05 00:33:42 +1000

    add arguments to startClient

--- http-service-agent/src/startClient.ts
+++ http-service-agent/src/startClient.ts
@@ -1,8 +1,35 @@
 import DispatchClient from "./DispatchClient.js";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
-const client = new DispatchClient({
-    accessKey: "12345",
-    dispatchServerBaseUrl: new URL("http://localhost:6701")
-});
+const argv = await yargs(hideBin(process.argv))
+    .env("APP_CFG")
+    .config()
+    .help()
+    .options({
+        accessKey: {
+            demandOption: true,
+            describe: "The remote dispatch server access key. basic auth format",
+            type: "string"
+        },
+        dispatchServerBaseUrl: {
+            describe: "the dispatch server base url.",
+            default: "http://localhost:6701",
+            coerce: (v) => new URL(v),
+            type: "string"
+        },
+        targetServerBaseUrl: {
+            demandOption: true,
+            describe: "Target workload server base url.",
+            coerce: (v) => new URL(v),
+            type: "string"
+        }
+    }).argv;
+
+const client = new DispatchClient(argv);
 
 await client.start();
+
+process.on("uncaughtException", (err, origin) => {
+    console.error(`Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+});
